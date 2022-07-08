4dae0d4eef04a59a3adccc6840a68b71f0ee0fa7
saskiamaulansyah <saskiamaulansyah@gmail.com>
2022-07-08 12:10:18 +0700

    Add splashscreen

---
+++ app/src/main/java/com/example/adoptme/Splash.java
@@ -0,0 +1,36 @@
+package com.example.adoptme;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+public class Splash extends Activity{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash);
+        Thread timer = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(4000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    finish();
+
+                    Intent m = new Intent(Splash.this,
+
+                            MainActivity.class);
+                    startActivityForResult(m,0);
+                }
+            }
+        };
+        timer.start();
+    }}
