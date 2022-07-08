78524036a678e230dc79c7a45169e6370e7c39b6
saskiamaulansyah <saskiamaulansyah@gmail.com>
2022-07-08 21:23:49 +0700

    Update Login

--- app/src/main/java/com/example/adoptme/WelcomeScreen.java
+++ app/src/main/java/com/example/adoptme/WelcomeScreen.java
@@ -2,8 +2,14 @@ package com.example.adoptme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -14,5 +20,14 @@ public class WelcomeScreen extends AppCompatActivity {
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         getSupportActionBar().hide();
+
+        Button Button1 = (Button)findViewById(R.id.button);
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iMenu1 = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(iMenu1);
+            }
+        });
     }
 }
