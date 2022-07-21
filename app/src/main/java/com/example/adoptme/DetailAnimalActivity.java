6c8117524ca736d7460d2006551c7ed8839edfd8
bimapw4 <bimapw4@gmail.com>
2022-07-21 13:54:17 +0700

    add detail animal

---
+++ app/src/main/java/com/example/adoptme/DetailAnimalActivity.java
@@ -0,0 +1,14 @@
+package com.example.adoptme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailAnimalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_animal);
+    }
+}
